04a608dae5a0d95fee358d40321589bbf88d3480
texco10 <ifeprecious20@gmail.com>
2022-09-11 08:15:59 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // initializing strings
+    char str[] = "Programming is like building a multilingual puzzle";
+
+    // writting to stdout
+    puts(str);
+
+    return 0;
+}
