ad9e79ba5338cad4fad1fa098a7102f101e69f5a
texco10 <ifeprecious20@gmail.com>
2022-09-15 18:24:24 -0700

    Isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,33 +3,20 @@
 
 
 /**
-
  * _isdigit - checks for uppercase character
-
  * @c: Variable text
-
  * Return: Always 0.
-
  */
 
 int _isdigit(int c)
   
 {
-  
   if (c >= 'A' && c <= 'Z')
-    
     {
-      
-      return (1);
-      
+      return (1); 
     }
-  
   else
-    
     {
-      
-      return (0);
-      
+      return (0); 
     }
-  
 }
